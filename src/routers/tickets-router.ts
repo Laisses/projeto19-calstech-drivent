1e2eb93dfc88ed98aa059b5657188230bde72332
Laisses <laisse@gmail.com>
2023-01-29 15:44:14 -0300

    feat: create a body schema validation to post tickets

--- src/routers/tickets-router.ts
+++ src/routers/tickets-router.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import { authenticateToken } from "@/middlewares";
+import { authenticateToken, validateBody } from "@/middlewares";
 import { getTicketsByTypes, getTicketByCustomer } from "@/controllers";
+import { ticketSchema } from "@/schemas";
 
 const ticketsRouter = Router();
 
 //ticketsRouter.all("/*", authenticateToken);
 ticketsRouter.get("/types", getTicketsByTypes);
 ticketsRouter.get("/", getTicketByCustomer);
-ticketsRouter.post("/");
+ticketsRouter.post("/", validateBody(ticketSchema));
 
 export { ticketsRouter };
