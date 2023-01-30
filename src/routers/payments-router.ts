eb7c22728adbbe821a3e314fd7586ffccd1e01db
Laisses <laisse@gmail.com>
2023-01-30 11:23:16 -0300

    feat: create payment schema validation

--- src/routers/payments-router.ts
+++ src/routers/payments-router.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import { authenticateToken, validateBody } from "@/middlewares";
 import { getPaymentInfo } from "@/controllers";
-
-//import { ticketSchema } from "@/schemas";
+import { paymentSchema } from "@/schemas";
 
 const paymentsRouter = Router();
 
-paymentsRouter.all("/*", authenticateToken);
+//paymentsRouter.all("/*", authenticateToken);
 paymentsRouter.get("/", getPaymentInfo);
-paymentsRouter.post("/process");
+paymentsRouter.post("/process", validateBody(paymentSchema));
 
 export { paymentsRouter };
