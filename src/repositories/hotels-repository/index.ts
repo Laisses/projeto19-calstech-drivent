87cb6d86c5e0e02c3feedf11ff98c2712d05911c
Laisses <laisse@gmail.com>
2023-02-01 09:31:04 -0300

    feat: add findMany and findHotelById functions to hotel repository

--- src/repositories/hotels-repository/index.ts
+++ src/repositories/hotels-repository/index.ts
@@ -4,3 +4,19 @@ import { Prisma } from "@prisma/client";
 export const selectHotels = async () => {
   return prisma.hotel.findMany();
 }
+
+export const selectHotelById = async (hotelId: number) => {
+  return prisma.hotel.findFirst({
+    where: {
+      id: hotelId
+    }
+  });
+};
+
+export const selectRoomsByHotel = async (hotelId: number) => {
+  return prisma.room.findMany({
+    where: {
+      hotelId
+    }
+  })
+};
