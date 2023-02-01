99243733143887fa067783d04227f2708592c083
Laisses <laisse@gmail.com>
2023-02-01 08:04:51 -0300

    feat: create the /hotels and the /hotels/:id routes

--- src/routers/hotels-router.ts
+++ src/routers/hotels-router.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
-import { authenticateToken } from "@/middlewares";
-import { getTicketsByTypes, getTicketByCustomer, createNewTicket } from "@/controllers";
-import { ticketSchema } from "@/schemas";
+//import { authenticateToken } from "@/middlewares";
+import { showHotels, showHotelById } from "@/controllers";
 
 const hotelsRouter = Router();
 
 //hotelsRouter.all("/*", authenticateToken);
-hotelsRouter.get("/");
-hotelsRouter.get("/:hotelId");
+hotelsRouter.get("/", showHotels);
+hotelsRouter.get("/:hotelId", showHotelById);
 
 export { hotelsRouter };
