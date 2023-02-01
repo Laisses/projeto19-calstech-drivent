b06915d4ca3a851c05afe739a5ffcb386a6f0fb0
Laisses <laisse@gmail.com>
2023-02-01 09:31:44 -0300

    refactor: remove coments

--- src/routers/hotels-router.ts
+++ src/routers/hotels-router.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-//import { authenticateToken } from "@/middlewares";
+import { authenticateToken } from "@/middlewares";
 import { showHotels, showHotelById } from "@/controllers";
 
 const hotelsRouter = Router();
 
-//hotelsRouter.all("/*", authenticateToken);
+hotelsRouter.all("/*", authenticateToken);
 hotelsRouter.get("/", showHotels);
 hotelsRouter.get("/:hotelId", showHotelById);
 
