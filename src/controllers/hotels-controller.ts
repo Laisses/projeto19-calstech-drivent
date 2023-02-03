3529d110f030090fe5587bdcc87929b10018a584
Laisses <laisse@gmail.com>
2023-02-03 17:02:42 -0300

    fix: add await before findHotelRooms and findHotels functions

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -3,15 +3,18 @@ import { AuthenticatedRequest } from "@/middlewares";
 import httpStatus from "http-status";
 import { Response } from "express";
 
-export const showHotels = (req: AuthenticatedRequest, res: Response) => {
+export const showHotels = async (req: AuthenticatedRequest, res: Response) => {
     const { userId } = req;
 
     try {
-        const hotels = findHotels(userId);
+        const hotels = await findHotels(userId);
         res.status(httpStatus.OK).send(hotels);
     } catch (error) {
-        if (error.name === "TicketNotPaid" || error.name === "TicketDoesNotIncludesHotel") {
+        if (error.name === "TicketDoesNotIncludesHotel") {
             res.sendStatus(httpStatus.FORBIDDEN);
+        }
+        if(error.name === "TicketNotPaid") {
+            res.sendStatus(httpStatus.PAYMENT_REQUIRED);
         }
         if (error.name === "TicketNotFound" || error.name === "NoEnrollmentFound") {
             res.sendStatus(httpStatus.NOT_FOUND);
@@ -19,12 +22,12 @@ export const showHotels = (req: AuthenticatedRequest, res: Response) => {
     }
 };
 
-export const showHotelById = (req: AuthenticatedRequest, res: Response) => {
+export const showHotelById = async (req: AuthenticatedRequest, res: Response) => {
     const { hotelId } = req.params;
     const { userId } = req;
 
     try {
-        const hotelRooms = findHotelRooms(userId, Number(hotelId));
+        const hotelRooms = await findHotelRooms(userId, Number(hotelId));
         res.status(httpStatus.OK).send(hotelRooms);
     } catch (error) {
         if (error.name === "TicketNotPaid" || error.name === "TicketDoesNotIncludesHotel") {
