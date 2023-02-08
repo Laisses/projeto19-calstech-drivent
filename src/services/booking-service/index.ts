46978f2ba2b89824ffe88e87f8eaa45778a85e66
Laisses <laisse@gmail.com>
2023-02-08 10:54:11 -0300

    refactor: remove validations to an outside function

--- src/services/booking-service/index.ts
+++ src/services/booking-service/index.ts
@@ -1,15 +1,26 @@
+import { selectHotels, selectHotelById, selectRoomsByHotel } from "@/repositories/hotels-repository";
 import { selectBooking, selectRoom } from "@/repositories/booking-repository";
 import { bookingNotFound, roomNotFound } from "./errors";
 
-export const findBooking = async (userId: number) => {
+const bookingValidation = async (userId: number) => {
     const booking = await selectBooking(userId);
     if (!booking) throw bookingNotFound();
+    return booking;
+};
 
-    const room = await selectRoom(booking.roomId);
+const roomValidation = async (roomId: number) => {
+    const room = await selectRoom(roomId);
     if (!room) throw roomNotFound();
+    return room;
+};
+
+export const findBooking = async (userId: number) => {
+    const booking = await bookingValidation(userId);
+    const room = await roomValidation(booking.id);
 
     return {
         id: booking.id,
         Room: room,
     }
 };
+
