8796cdabb18e6e96b63a10e6ccb317ec63af51cf
Laisses <laisse@gmail.com>
2023-02-08 10:23:38 -0300

    feat: create a booking validation middleware

---
+++ src/middlewares/booking-middleware.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Response } from "express";
+import { AuthenticatedRequest } from "@/middlewares";
+import enrollmentRepository from "@/repositories/enrollment-repository";
+import { findTickects, findTickectByType } from "@/repositories/tickets-repository";
+import httpStatus from "http-status";
+
+export const validateBooking = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    const {userId} = req;
+
+    const enrollment = await enrollmentRepository.findUserEnrollment(userId);
+    if (!enrollment) return res.sendStatus(httpStatus.NOT_FOUND);
+
+    const ticket = await findTickects(enrollment.id);
+    if (!ticket) return res.sendStatus(httpStatus.FORBIDDEN);
+    if (ticket.status === "RESERVED") return res.sendStatus(httpStatus.FORBIDDEN);
+
+    const ticketType = await findTickectByType(ticket.ticketTypeId);
+    if (ticketType.isRemote === false) return res.sendStatus(httpStatus.FORBIDDEN);
+    if (ticketType.includesHotel === false) return res.sendStatus(httpStatus.FORBIDDEN);
+
+};
