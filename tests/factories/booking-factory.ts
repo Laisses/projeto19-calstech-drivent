26e533db37e86169d85068df82da9355ec3a7303
Laisses <laisse@gmail.com>
2023-02-09 19:37:26 -0300

    test: add create validBooking function

--- tests/factories/booking-factory.ts
+++ tests/factories/booking-factory.ts
@@ -1,11 +1,11 @@
 import faker from "@faker-js/faker";
 import { prisma } from "@/config";
 
-export async function createInvalidBooking(userId: number) {
+export async function createValidBooking(userId: number, roomId: number) {
     return prisma.booking.create({
         data: {
             userId,
-            roomId: 0
+            roomId,
         }
     });
 }
